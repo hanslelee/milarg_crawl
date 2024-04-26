ae06ab45ab082c02fdf1f2b12d20e2efe2a314f3
hanslelee <hssarah13@gmail.com>
2024-04-26 21:37:11 +0900

    pageLimit 반복문 range 잘못 설정 오류 보완

--- crawl.py
+++ crawl.py
@@ -6,13 +6,17 @@ import requests
 from bs4 import BeautifulSoup
 from utils import eraseSpace
 
-# pageLimit = 204
-pageLimit = 2
+import urllib3
+urllib3.disable_warnings()
+
+pageLimit = 204
 articleNumPerPage = 10
 
 f = open('./mil_data_raw.txt', 'w')
 
-for pageNum in range(1, int(pageLimit) * 10, 10): # 1에서 100까지 10씩 증가 (1, 11, 21, ..., 91)
+for pageNum in range(1, pageLimit): # 1에서 100까지 10씩 증가 (1, 11, 21, ..., 91)
+    print('pageNum : '+str(pageNum))
+    
     # 한글은 이렇게 인코딩되어 나타남
     res = requests.get(f"https://www.kida.re.kr/frt/board/frtNormalBoard.do?searchCondition=title&searchKeyword=&pageIndex={pageNum}&depth=3&sidx=382&stype=&v_sidx=", verify=False)
     htmlCode = res.text
